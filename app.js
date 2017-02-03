4961af17e771111e9dfa0ee6583cfed0b54beae5
Yuriy <bo20202@yandex.ru>
2017-02-03 18:56:52 +0000

    add helmet.js middleware

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var helmet = require('helmet')
 
 var index = require('./src/routes/index');
 var users = require('./src/routes/users');
@@ -19,6 +19,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(helmet())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
