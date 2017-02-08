cfb3d13a25365ea42ddff905610df599c31d1697
Yuriy <bo20202@yandex.ru>
2017-02-08 19:40:59 +0000

    Anohter app structure refactor

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -1,31 +1,7 @@
 const fs = require("fs")
 const path = require("path")
 const Sequelize = require("sequelize")
-const env = process.env.NODE_ENV || 'development'
-const config = require("../config/db_config")
-
-let sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-    define: {
-        timestamps: false,
-        underscored: true
-    },
-    pool: {
-        max: 10,
-        min: 0,
-        idle: 10000
-    },
-    hooks:{
-        beforeDefine(attributes, options){
-            Object.keys(attributes).forEach((attr) => {
-                if(attributes[attr].allowNull != true){
-                    attributes[attr].allowNull = false
-                }
-            })
-        }
-    }
-})
+const sequelize = require(path.resolve("src/lib/db/db"))
 
 let db = {}
 
